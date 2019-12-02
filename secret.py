a671e64110e50a1bc9ea8ee2b227b5d824184fb2
bobmoran2 <40918805+bobmoran2@users.noreply.github.com>
2019-12-02 18:48:00 +0100

    Create secret.py

---
+++ secret.py
@@ -0,0 +1,11 @@
+{
+    "values": [
+        {
+            "name": "client_id",
+            "value": "AKIA6NH2YJ88R1RMTRVO"
+        },
+        {
+            "name": "client_secret",
+            "value": "1mZvD8jZGOCVUpInXPChXPIhHQL43sjcfxYQ8/cb"
+        }
+    ],
